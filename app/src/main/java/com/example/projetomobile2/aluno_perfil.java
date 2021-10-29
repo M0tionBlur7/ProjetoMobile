f9fda4e4b3cc8f7d51b754a514f7e004b6a1d8c9
Samuel <samvi28@hotmail.com>
2021-10-29 16:49:36 -0300

    teste

---
+++ app/src/main/java/com/example/projetomobile2/aluno_perfil.java
@@ -0,0 +1,14 @@
+package com.example.projetomobile2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aluno_perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aluno_perfil);
+    }
+}
---
+++ app/src/main/java/com/example/projetomobile2/reg_freq.java
@@ -0,0 +1,14 @@
+package com.example.projetomobile2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reg_freq extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_freq);
+    }
+}
